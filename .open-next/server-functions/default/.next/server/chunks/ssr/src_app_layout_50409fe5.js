53a455918acba1aeb629fb265bf54db1f27b241a
wes337 <wesmoses@gmail.com>
2026-02-05 09:04:41 +0200

    git ignore

--- .open-next/server-functions/default/.next/server/chunks/ssr/src_app_layout_50409fe5.js
+++
@@ -1,3 +0,0 @@
-module.exports=[29735,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsx)("body",{className:"antialiased",children:a})})}a.s(["default",()=>c,"metadata",0,{title:"Lip Critic",description:"The official website for NYC-based electronic punk band Lip Critic."}])}];
-
-//# sourceMappingURL=src_app_layout_50409fe5.js.map
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mVT+RGNWW+1/s/uNtN0FQHI1KgFHAx4nqbigm7fRq0Q=\"\n}"
